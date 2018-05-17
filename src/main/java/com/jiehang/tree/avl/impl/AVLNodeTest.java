9cb4b06174e1f4aeaa159b1a11a41f4430dfc8f8
jiehang <jiehang@123.com>
2018-05-17 23:58:40 +0800

    fisrt commit

---
+++ src/main/java/com/jiehang/tree/avl/impl/AVLNodeTest.java
@@ -0,0 +1,24 @@
+package com.jiehang.tree.avl.impl;
+
+public class AVLNodeTest {
+	public static void main(String[] args) {
+		AVLTree<Integer> avlTree = new AVLTree<Integer>();
+		int[] arryaInt = new int[]{42,35,19,14,10,27,31};
+		for(int i=0; i<arryaInt.length; i++) {
+			avlTree.insert(arryaInt[i]);
+		}
+		
+		display(avlTree.root);
+	}
+	
+	public static void display(AVLNode<Integer> avlNode) {
+		if (avlNode != null) {
+			display(avlNode.left);
+			display(avlNode.right);
+			System.out.println(avlNode.data + ":" + avlNode.height);
+			System.out.println("left :" + ((avlNode.left != null) ? avlNode.left.data : "null"));
+			System.out.println("right :" + ((avlNode.right != null) ? avlNode.right.data : "null"));
+		}
+	}
+	
+}
